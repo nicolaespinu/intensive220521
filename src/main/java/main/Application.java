4bec75e5e6ed6067610b5ac45e4d2131654148f5
Nicolae Spinu <11spinic@gmail.com>
2021-05-22 18:43:45 +0300

    ... needs work

--- src/main/java/main/Application.java
+++ src/main/java/main/Application.java
@@ -7,9 +7,5 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class);
-        System.out.println("spinic***************");;
     }
 }
-/*
-* https://live.skillbox.ru/webinars/code/znakomstvo-s-enterprise-razrabotkoi-pishem-korporativnyi-chat-na-java220521/?utm_source=expertsender&utm_medium=email&utm_campaign=all_all_expertsender_email_invite_intensive-281-2021-05_all_code_skillbox&utm_content=2021-05-22&utm_term=intensive&email=11spinic%40gmail.com
-* */
